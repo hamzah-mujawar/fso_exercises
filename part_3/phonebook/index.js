bfad3984feff03e49193385c2126700b1477c335
Hamzah Mujawar <hamzah1faisal@gmail.com>
2025-11-10 22:02:23 +0000

    Finished exercise 3.15 - 3.18

--- part_3/phonebook/index.js
+++ part_3/phonebook/index.js
@@ -18,32 +18,55 @@ morgan.token('post', function(req, res) {
 		return ''
 })
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
 	Person.find({}).then(persons => {
 		res.json(persons)
 	})
+		.catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
-	const count = persons.length
-	const date = new Date;
-	res.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`)
-})
-
-app.get('/api/persons/:id', (req, res) => {
-	Person.findById(request.params.id).then(person => {
-		res.json(person)
+app.get('/info', (req, res, next) => {
+	Person.find({}).then(person => {
+		const count = person.length
+		const date = new Date;
+		res.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`)
 	})
+		.catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-	const id = req.params.id
-	persons = persons.filter(person => person.id !== id)
-
-	res.status(204).end()
+app.get('/api/persons/:id', (req, res, next) => {
+	Person.findById(req.params.id)
+		.then(person => {
+			if (person)
+				res.json(person)
+			else
+				res.status(404).end()
+		})
+		.catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
+	Person.findByIdAndDelete(req.params.id)
+		.then(result => {
+			res.status(204).end()
+		})
+		.catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+	const body = req.body
+	const person = {
+		name: body.name,
+		number: body.number,
+	}
+	Person.findByIdAndUpdate(req.params.id, person)
+		.then(result => {
+			res.json(person)
+		})
+		.catch(error => next(error))
+})
+
+app.post('/api/persons', (req, res, next) => {
 	const body = req.body
 
 	if (!body.number) {
@@ -66,10 +89,21 @@ app.post('/api/persons', (req, res) => {
 	person.save().then(savedPerson => {
 		res.json(savedPerson)
 	})
+		.catch(error => next(error))
 
 })
 
+const errorHandler = (error, req, res, next) => {
+	console.error(error.message)
 
+	if (error.name === 'CastError') {
+		return res.status(400).send({ error: 'malformatted id' })
+	}
+
+	next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
